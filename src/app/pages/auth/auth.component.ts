f92c2340b84fdfb23cc8ef4b147a64846c7b2c90
HassanRaza <rayanulhassan@outlook.com>
2025-04-14 20:47:21 +0500

    auth empty pages created

---
+++ src/app/pages/auth/auth.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  imports: [],
+  templateUrl: './auth.component.html',
+  styles: ``
+})
+export class AuthComponent {
+
+}
---
+++ src/app/shared/components/sign-in/sign-in.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in',
+  imports: [],
+  templateUrl: './sign-in.component.html',
+  styles: ``
+})
+export class SignInComponent {
+
+}
