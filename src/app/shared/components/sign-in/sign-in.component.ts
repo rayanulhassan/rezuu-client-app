4c9e079711b236c7765bdf5f9df217d8b20e613c
HassanRaza <rayanulhassan@outlook.com>
2025-04-14 22:06:06 +0500

    auth pages ui complete

--- src/app/shared/components/sign-in/sign-in.component.ts
+++ src/app/shared/components/sign-in/sign-in.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { ReactiveFormsModule, FormsModule, FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [ButtonModule, InputTextModule, ReactiveFormsModule, FormsModule],
   templateUrl: './sign-in.component.html',
   styles: ``
 })
 export class SignInComponent {
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
+  signIn() {
+    console.log(this.form.value);
+  }
 }
--- src/app/shared/components/sign-up/sign-up.component.ts
+++ src/app/shared/components/sign-up/sign-up.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { ReactiveFormsModule, FormsModule, FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [ButtonModule, InputTextModule, ReactiveFormsModule, FormsModule],
   templateUrl: './sign-up.component.html',
   styles: ``
 })
 export class SignUpComponent {
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
 
+  signUp() {
+    console.log(this.form.value);
+  }
 }
