eb783001a8404a403a98f39f3eab759d0e017f8d
HassanRaza <rayanulhassan@outlook.com>
2025-04-14 19:58:49 +0500

    installed angular/fire library for firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,9 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import { RezuuPreset } from './rezuu-primeng-preset';
-
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -25,5 +27,18 @@ export const appConfig: ApplicationConfig = {
         },
       },
     }),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'rezuu-8a4c9',
+        appId: '1:874346793605:web:c06142c4a6f41f018ef7ec',
+        storageBucket: 'rezuu-8a4c9.appspot.com',
+        apiKey: 'AIzaSyC60ptrrzka61FD-hY0FSfFctjDUWCCWJs',
+        authDomain: 'rezuu-8a4c9.firebaseapp.com',
+        messagingSenderId: '874346793605',
+        measurementId: 'G-JHH3RZXW55',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 };
