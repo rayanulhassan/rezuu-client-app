baa6b0e0ae876696c2652ee51faeba535cd722f0
HassanRaza <rayanulhassan@outlook.com>
2025-05-04 00:05:18 +0500

    Update favicon and enhance default layout with responsive user greeting and navigation toggle functionality. Add new favicon links in index.html and implement menu toggle in default-layout component for improved user experience.

--- src/app/shared/components/layouts/default-layout/default-layout.component.ts
+++ src/app/shared/components/layouts/default-layout/default-layout.component.ts
@@ -1,19 +1,31 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet, RouterLink, Router } from '@angular/router';
+import { RouterOutlet, RouterLink, Router, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
 import { Toast } from 'primeng/toast';
 import { FooterComponent } from "../../footer/footer.component";
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-default-layout',
-  imports: [RouterOutlet, RouterLink, Toast, FooterComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, Toast, FooterComponent],
   templateUrl: './default-layout.component.html',
   styles: ``
 })
 export class DefaultLayoutComponent {
   #authService = inject(AuthService);
   #router = inject(Router);
+  #userService = inject(UserService);
 
+  userDetails = this.#userService.userDetails;
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  handleNavigation() {
+    this.isMenuOpen = false;
+  }
 
   logout() {
     this.#authService.logout();
