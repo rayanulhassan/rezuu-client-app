ad5fe2292cc94d23c56f92dd5a341908699fd360
HassanRaza <rayanulhassan@outlook.com>
2025-05-14 23:32:47 +0500

    added videos on how it works page

--- src/app/pages/how-it-works/how-it-works.component.ts
+++ src/app/pages/how-it-works/how-it-works.component.ts
@@ -4,8 +4,13 @@ import { RouterModule } from '@angular/router';
   selector: 'app-how-it-works',
   imports: [RouterModule],
   templateUrl: './how-it-works.component.html',
-  styles: ``
+  styles: ``,
 })
 export class HowItWorksComponent {
-
+  videoUrls = [
+    'https://rezuu-assets.tor1.cdn.digitaloceanspaces.com/app-assets/rezuu-create-profile-step-1.mp4',
+    'https://rezuu-assets.tor1.cdn.digitaloceanspaces.com/app-assets/rezuu-create-profile-step-2.mp4',
+    'https://rezuu-assets.tor1.cdn.digitaloceanspaces.com/app-assets/rezuu-create-profile-step-3.mp4',
+    'https://rezuu-assets.tor1.cdn.digitaloceanspaces.com/app-assets/rezuu-create-profile-step-4.mp4',
+  ];
 }
